0224a5fc6d9c35fa0267e6658ba672405cac61df
Claudia Smriglio <smriglioc@gmail.com>
2025-04-16 23:20:55 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // 👈 tell Vite to put the build output in "dist"
+    outDir: 'dist', 
   },
 })
